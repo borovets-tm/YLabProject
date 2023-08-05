5f329bf5b00cdd1386910c49b65a5fa7a9839683
Тимофей Боровец <t.m.borovets@icloud.com>
2023-08-05 21:24:56 +0300

    databases for tests and for the application are separated

--- menu_app/database.py
+++ menu_app/database.py
@@ -7,15 +7,16 @@ from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
+TEST: bool = getenv('TEST', '0') == '1'
 POSTGRES_USER: str | None = getenv('POSTGRES_USER', 'default')
 POSTGRES_PASSWORD: str | None = getenv('POSTGRES_PASSWORD')
-POSTGRES_DB: str | None = getenv('POSTGRES_DB')
 PORT: str | None = getenv('PORT')
 HOST_DB: str | None = getenv('HOST_DB')
+USED_DB: str | None = getenv('TEST_DB') if TEST else getenv('POSTGRES_DB')
 
 SQLALCHEMY_URL: str = (
     f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}'
-    f'@{HOST_DB}:{PORT}/{POSTGRES_DB}'
+    f'@{HOST_DB}:{PORT}/{USED_DB}'
 )
 
 engine: Engine = create_engine(SQLALCHEMY_URL)
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -2,14 +2,15 @@ from os import getenv
 from uuid import UUID
 
 import aioredis
+import dotenv
 import pytest
-from dotenv import load_dotenv
 from fastapi.testclient import TestClient
 
 from menu_app.main import app
 
 client = TestClient(app=app)
-load_dotenv()
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
 REDIS_HOST = getenv('REDIS_HOST', 'localhost')
 url_redis = f'redis://{REDIS_HOST}/2'
 
@@ -38,6 +39,14 @@ async def get_entity_id(key: str) -> UUID:
     return entity_id
 
 
+@pytest.mark.order(0)
+@pytest.mark.asyncio
+async def test_change_db_on_test():
+    dotenv.set_key(dotenv_file, 'TEST', '1')
+    test_value = dotenv.get_key(dotenv_file, 'TEST')
+    assert test_value == '1'
+
+
 @pytest.mark.order('last')
 @pytest.mark.asyncio
 async def test_clear_cache_after_test():
@@ -46,3 +55,6 @@ async def test_clear_cache_after_test():
     keys = await redis.keys()
     await redis.connection_pool.disconnect()
     assert keys == []
+    dotenv.set_key(dotenv_file, 'TEST', '0')
+    test_value = dotenv.get_key(dotenv_file, 'TEST')
+    assert test_value == '0'
