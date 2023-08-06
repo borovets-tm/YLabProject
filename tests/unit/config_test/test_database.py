446c0cac47e5a1bafa6272dc8e934d8db16fcc8f
Тимофей Боровец <t.m.borovets@icloud.com>
2023-08-06 18:41:51 +0300

    fixed incorrect operation of the database separation for tests and applications

--- tests/unit/config_test/test_database.py
+++ tests/unit/config_test/test_database.py
@@ -4,14 +4,17 @@ from typing import Generator
 
 from dotenv import load_dotenv
 from sqlalchemy import Engine, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
+
+from menu_app.database import get_db
+from menu_app.main import app
 
 load_dotenv()
 POSTGRES_USER: str | None = getenv('POSTGRES_USER', 'default')
 POSTGRES_PASSWORD: str | None = getenv('POSTGRES_PASSWORD')
 PORT: str | None = getenv('PORT')
 HOST_DB: str | None = getenv('HOST_DB')
-USED_DB: str | None = getenv('POSTGRES_DB')
+USED_DB: str | None = getenv('TEST_DB')
 
 SQLALCHEMY_URL: str = (
     f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}'
@@ -19,18 +22,20 @@ SQLALCHEMY_URL: str = (
 )
 
 engine: Engine = create_engine(SQLALCHEMY_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db() -> Generator:
+def override_get_db() -> Generator:
     """
-    Генератор сессии взаимодействия с базой данных.
+    Генератор тестовой сессии взаимодействия с базой данных.
 
     :return: Экземпляром сеанса базы данных.
     """
-    db = SessionLocal()
+    db = TestSessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
